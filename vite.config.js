713ea380f39d2e58a7f08960e90db7201e52f91c
nzmort <118791096+nozomuorita@users.noreply.github.com>
2023-11-27 10:59:27 +0900

    hoge

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES  // この行を追加
-  ? "test"            // この行を追加
+  ? "atcoder-game1"            // この行を追加
   : "./",                     // この行を追加
   plugins: [react()],
 })
